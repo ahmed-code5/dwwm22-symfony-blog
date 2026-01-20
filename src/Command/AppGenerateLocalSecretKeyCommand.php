13d3586a9f7d4753d7e15faa6c6aa946bfd1fbfb
ahmed-code5 <sahalayeh95@gmail.com>
2026-01-20 13:59:51 +0100

    Add PHP-CS-Fixer and apply style fixes

--- src/Command/AppGenerateLocalSecretKeyCommand.php
+++ src/Command/AppGenerateLocalSecretKeyCommand.php
@@ -4,9 +4,7 @@ namespace App\Command;
 
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
@@ -16,26 +14,25 @@ use Symfony\Component\Console\Style\SymfonyStyle;
 )]
 class AppGenerateLocalSecretKeyCommand extends Command
 {
-    
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
-       
-        $enviFile='.env.dev.local';
+
+        $enviFile = '.env.dev.local';
 
         if (file_exists($enviFile)) {
             $io->error($enviFile.' file already exists. Aborting to avoid overwriting existing file.');
+
             return Command::FAILURE;
         }
 
-       bin2hex(random_bytes(16));
+        bin2hex(random_bytes(16));
 
         $appSecret = bin2hex(random_bytes(16));
-        
+
         file_put_contents($enviFile, "APP_SECRET=$appSecret\n");
         $io->success($enviFile.' file created with a new APP_SECRET key.');
-        
-    
-     return Command::SUCCESS;
- }
+
+        return Command::SUCCESS;
+    }
 }
